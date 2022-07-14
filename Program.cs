1ddb675cc7f347020995d2b4465e83670fd30a1d
Ayub254 <jobkarani084j@gmail.com>
2022-07-14 13:32:25 +0300

    looping thro arrays

--- Program.cs
+++ Program.cs
@@ -9,12 +9,38 @@ namespace GradeBook{
         
         static void Main(string [] args){
 
-            double x = 3.14;
-            double y = 4.35;
 
-            double z = x + y;
+// sum 
+            // double x = 3.14;
+            // double y = 4.35;
 
-            Console.WriteLine(z);
+            // var x = 3.14;
+            // var y = 4.35;
+
+            // var z = x + y;
+
+// Array 
+            // var numbers = new double[3];
+            // numbers[0] = 1.23;
+            // numbers[1] = 2.23;
+            // numbers[2] = 6.23;
+
+// Array init 
+           var numbers = new [] {1.23, 2.00, 6.23}; 
+
+        //    var results = numbers[0];
+        //    results += numbers[1];
+        //    results += numbers[2];
+
+            var results = 0.0;
+            
+            foreach(double number in numbers){
+
+                results += number;
+
+            }
+
+            Console.WriteLine(results);
             
             if(args.Length > 0){
 
