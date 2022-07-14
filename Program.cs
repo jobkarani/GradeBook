4870f3da9d770c68bbbbdf2a4bcb076c43e6c177
Ayub254 <jobkarani084j@gmail.com>
2022-07-14 08:38:25 +0300

    sum of x and y: ;

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace GradeBook{
+
+    class Program{
+        
+        static void Main(string [] args){
+
+            double x = 3.14;
+            double y = 4.35;
+
+            double z = x + y;
+
+            Console.WriteLine(z);
+            
+            if(args.Length > 0){
+
+                Console.WriteLine($"Hello, {args[0]} !");
+            }
+            else{
+                Console.WriteLine("Hello");
+            }
+        }
+    }
+}                                                                                          
